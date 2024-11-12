009ce5324b6af19bdcb0bc7f61b41c48020f688f
Edvard Sire <mail@edvardsire.com>
2024-11-12 10:35:16 +0100

    old_keras -> shell.nix

--- shell.nix
+++ shell.nix
@@ -1,4 +1,9 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (builtins.fetchGit {
+    url = "https://github.com/NixOS/nixpkgs.git";
+    rev = "057f9aecfb71c4437d2b27d3323df7f93c010b7e";
+    ref = "release-23.11";
+  }) {} }:
+
 pkgs.mkShell {
   buildInputs = (with pkgs.python311Packages; [
       numpy
