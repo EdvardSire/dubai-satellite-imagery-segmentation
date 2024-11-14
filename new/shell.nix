939f77c14adc95fada84dc7a3fad8293ab9758e3
Edvard Sire <mail@edvardsire.com>
2024-11-14 17:48:17 +0100

    chore: new shell

--- new/shell.nix
+++ new/shell.nix
@@ -1,21 +1,16 @@
-{ pkgs ? import (builtins.fetchGit {
-    url = "https://github.com/NixOS/nixpkgs.git";
-    rev = "057f9aecfb71c4437d2b27d3323df7f93c010b7e";
-    ref = "release-23.11";
-  }) {} }:
+{ pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-  buildInputs = (with pkgs.python311Packages; [
+  buildInputs = (with pkgs.python312Packages; [
       numpy
-      scipy
-      matplotlib
-      pandas
-      albumentations
-      # torch
-      tensorflow
-      keras
-      notebook
-      nbformat
+      # matplotlib
+      # pandas
+      # albumentations
+      torch
+      # tensorflow
+      # keras
+      # notebook
+      # nbformat
   ]);
 }
 
