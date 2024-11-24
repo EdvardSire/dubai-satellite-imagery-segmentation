a636480a65e0781fb486ddf10f62f4f0d202f3d4
Edvard Sire <mail@edvardsire.com>
2024-11-24 20:39:19 +0100

    chore

--- new/shell.nix
+++ new/shell.nix
@@ -1,16 +1,23 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {config = {allowUnfree = true; }; } }:
 
 pkgs.mkShell {
-  buildInputs = (with pkgs.python312Packages; [
-      numpy
-      # matplotlib
-      # pandas
-      # albumentations
-      torch
-      # tensorflow
-      # keras
-      # notebook
-      # nbformat
+  buildInputs = (with pkgs; [
+      (python312.withPackages (ps: with ps; [
+                               (ps.opencv4.override {
+                                enableGtk3 = true;
+                                enableUnfree = true;
+                                })
+      ]))
+
+
+  ]) ++ (with pkgs.python312Packages; [
+    numpy
+    torch
+    torchvision
+    onnx
+    timm
+    ipython
+    albumentations
   ]);
 }
 
