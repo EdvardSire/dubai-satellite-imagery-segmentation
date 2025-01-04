7ce637d2b3e2d676c5cb2a653748e89b0987f809
Edvard Sire <mail@edvardsire.com>
2025-01-04 17:34:34 +0100

    corrupt images with torchvision.io.decode_image

--- shell.nix
+++ shell.nix
@@ -13,7 +13,8 @@ pkgs.mkShell {
   nativeBuildInputs = with pkgs.buildPackages; [
     cuda-python312
     python312Packages.torchWithCuda
-    # python312Packages.torchvision
+    python312Packages.torchvision
+    python312Packages.ipython
     (python312.withPackages (ps: with ps; [
                              (ps.opencv4.override {
                               enableGtk3 = true;
--- src/dubai_dataset.py
+++ src/dubai_dataset.py
@@ -1,24 +1,68 @@
 from pathlib import Path
-import torch
 from torch.utils.data import Dataset
+from torchvision.io import decode_image
+import cv2
+import torch
+import sys
 
-class DubaiImageDataset(Dataset):
+
+def show_torchvision_image(image, window_name="window"):
+    image = image.permute(1, 2, 0).numpy()  # Convert to (H, W, C)
+    cv2.imshow(window_name, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+class DubaiDatasetBatchless(Dataset):
     def __init__(self, folder_path):
-        super(DubaiImageDataset, self).__init__()
+        super(DubaiDatasetBatchless, self).__init__()
         self.img_files = list(Path(folder_path).glob('*.jpg'))
-        self.mask_files = []
-        for img_path in self.img_files:
-             self.mask_files.append(Path(img_path.parent, img_path.stem).with_suffix('.png'))
+        self.samples = []
+        for img_file in self.img_files:
+            mask_file = Path(img_file.parent, img_file.stem).with_suffix('.png')
+            self.samples.append((decode_image(img_file.__str__()), decode_image(mask_file.__str__())))
+
         self.classes = [60, 132, 110, 254, 226, 155]
 
-    def __getitem__(self, index):
-        return
 
+        show_torchvision_image(self.samples[46][1])
+
+        for sample in self.samples:
+            show_torchvision_image(sample[1])
+
+        # ### Run this snippet to get an idea of the mistakes in the masks
+        # # show_torchvision_image(self.samples[46][1])
+        # for index, sample in enumerate(self.samples):
+        #     first_image_channel = sample[1][0]
+        #     print(first_image_channel.shape)
+        #     tensor_to_match = torch.tensor(self.classes)
+        #     matches = (first_image_channel.unsqueeze(-1) == tensor_to_match).any(dim=-1)
+        #     non_matches_count = (~matches).sum().item()
+        #     print(f'{index} has {non_matches_count} non-matches')
+        #     print(first_image_channel[~matches])
+        #     print()
+        #     sys.stdout.flush()
+
+
+
+
+
+    def __getitem__(self, index):
+        return self.samples[index][0].unsqueeze_(0), None
 
 
     def __len__(self):
-        return 40
+        return len(self.samples)
 
 
 if __name__ == '__main__':
-    dataset = DubaiImageDataset('dataset/train')
+    dataset = DubaiDatasetBatchless(Path(__file__).parent.parent / 'dataset' / 'train')
+    img, label = dataset.__getitem__(0)
+    print(img.shape)
+
+
+    # img = decode_image(dataset.img_files[0].__str__())
+    # mask = decode_image(dataset.img_files[0].__str__().replace("jpg", "png"))
+    # show_torchvision_image(img)
+    # show_torchvision_image(mask)
+
+
